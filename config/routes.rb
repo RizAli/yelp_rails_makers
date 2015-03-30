260ae25c4cc7468e2c4ab1e624632701024591ca
Rizwan Ali <rizcube@gmail.com>
2015-03-30 12:48:25 +0100

    add restaurants view with link

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # require './app/controllers/restaurants_controller.rb'
+  require_relative '../app/controllers/restaurants_controller.rb'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -7,7 +9,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get 'restaurants' => 'restaurant#index'
+  get 'restaurants' => 'restaurants#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
